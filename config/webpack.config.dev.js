35f9b3b4d62ede39838a465603559cf6540a6426
ansarimofid <ansarimofid@gmail.com>
2017-05-31 14:34:09 +0530

    Chore: Adds tinygradients as dependency and configures webpack alias to use tinygradient2

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -79,7 +79,8 @@ module.exports = {
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web'
+      'react-native': 'react-native-web',
+      'tinycolor': "tinycolor2"
     }
   },
   
