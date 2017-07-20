78f40bdbac7da645690c746ce61ca78c9406d4c0
ansarimofid <ansarimofid@gmail.com>
2017-07-20 23:45:33 +0530

    style:adds navbar style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './Header.css'
+
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/collection'>Collection</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul>
-    </nav>
-  </header>
+  <div className="uk-container">
+    <header is uk-sticky="show-on-up: true;animation: uk-animation-slide-top;">
+      <nav class="uk-navbar uk-navbar-container uk-navbar-transparent">
+        <div class="uk-navbar-right">
+          <ul class="uk-navbar-nav">
+            <li class="uk-active"><Link to='/'>Home</Link></li>
+            <li><Link to='/collection'>Collection</Link></li>
+            <li><Link class="uk-padding-remove-right" to='/about'>About</Link></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </div>
 );
 
 export default Header
