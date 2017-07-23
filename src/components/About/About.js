6f1f97cea5406a2f3e82c8f73f189827e225744c
ansarimofid <ansarimofid@gmail.com>
2017-07-23 15:46:45 +0530

    Fix:Fixes .length error; Refactor:Adds error handing on no item in collection

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@
  */
 import React from 'react'
 const About = ()=>(
-  <div>
+  <div className="uk-container uk-text-center">
     <h2>I'm from About</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque debitis dolor eos esse magnam tempore voluptatibus! Distinctio dolor enim impedit nisi non officiis optio possimus veniam. Accusantium dolor ex fugiat id illum ipsa labore magnam nostrum quod temporibus. Deleniti distinctio dolorum eveniet nesciunt placeat quidem repellat vitae voluptate voluptatibus!</p></div>
 );
--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -3,6 +3,14 @@
  */
 import React, {Component} from 'react'
 import GradientCard from '../GradientCard/GradientCard'
+import { NavLink } from 'react-router-dom'
+
+const CollectionError = ()=> (
+  <div class="uk-text-center uk-width-1-1">
+    <h3>No item in collection</h3>
+    <p>Tip: Add item in collection by clicking 'save' button on any card on <NavLink exact to='/' activeClassName="active">Home</NavLink></p>
+    </div>
+);
 
 class Collection extends Component {
 
@@ -18,10 +26,14 @@ class Collection extends Component {
     // Fetch Gradients From Local Storage
     let gradientCollection = JSON.parse(localStorage.getItem("gradient-collection"));
     // Get array of random gradients cards
-    for (let i=0;i<gradientCollection.length;i++) {
-      cardArray.push(<GradientCard handleChange={this.handleChange.bind(this)} cardAction="remove"  gradientColors={gradientCollection[i]}/>);
+    if (gradientCollection !== null && gradientCollection.length) {
+      for (let i=0;i<gradientCollection.length;i++) {
+        cardArray.push(<GradientCard handleChange={this.handleChange.bind(this)} cardAction="remove"  gradientColors={gradientCollection[i]}/>);
+      }
+      return cardArray;
     }
-    return cardArray;
+
+    return false;
   }
 
   handleChange() {
@@ -36,8 +48,8 @@ class Collection extends Component {
         <h1 className="title-deco gradient-text uk-text-center uk-margin-medium-top uk-margin-medium-bottom">
           Saved-Collection
         </h1>
-        <div class="uk-grid">
-          {this.state.gradientCollections}
+        <div class="uk-grid uk-margin-medium-bottom">
+          {this.state.gradientCollections ?this.state.gradientCollections:<CollectionError/>}
         </div>
       </div>
     )
