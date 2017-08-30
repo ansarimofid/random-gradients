a8d1d97aae834b053f381a37b76386a773315499
ansarimofid <ansarimofid@gmail.com>
2017-08-30 18:48:47 +0530

    Updates about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,17 @@
 import React from 'react'
 const About = ()=>(
   <div className="uk-container uk-text-center">
-    <h2>I'm from About</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque debitis dolor eos esse magnam tempore voluptatibus! Distinctio dolor enim impedit nisi non officiis optio possimus veniam. Accusantium dolor ex fugiat id illum ipsa labore magnam nostrum quod temporibus. Deleniti distinctio dolorum eveniet nesciunt placeat quidem repellat vitae voluptate voluptatibus!</p></div>
+      <div is uk-grid>
+          <div className="uk-width-1-6@s uk-width-1-6@m"></div>
+          <div className="uk-width-2-3@s uk-width-2-3@m">
+              <h1 className="title-deco gradient-text uk-text-center uk-margin-medium-top uk-margin-medium-bottom">
+                  Random Thoughts
+              </h1>
+              <p className="uk-text-left">It started with an obsession for colours and gradients. There are millions of colours and possible gradient combination but we are not able to even explore 10% of them. There was a need for something which can make us explore every possibility of colours and gradients. With this thought, <span className="gradient-text">Random Gradients</span> was born.</p>
+              <p className="uk-text-left"> <span className="gradient-text">Random Gradients</span>  gives a designer possibility to explore infinite no. of a possibility of colours and gradients and even save them for further use in their design. </p>
+          </div>
+          </div>
+      </div>
 );
 
 export default About
