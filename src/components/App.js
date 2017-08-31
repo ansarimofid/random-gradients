e20e1cb84470040b13766070c7c32d5e3c0a53df
ansarimofid <ansarimofid@gmail.com>
2017-08-31 16:41:44 +0530

    Adds Footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@
 import React from 'react'
 import Main from './Main'
 import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 const App = () => (
   <div>
     <Header />
     <Main />
+    <Footer/>
   </div>
-)
+);
 
 export default App
