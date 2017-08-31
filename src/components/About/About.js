be2bcdf64dc3b300df2b08f3813c358df2ad5342
ansarimofid <ansarimofid@gmail.com>
2017-08-31 20:42:20 +0530

    Updates about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ const About = ()=>(
               <h1 className="title-deco gradient-text uk-text-center uk-margin-medium-top uk-margin-medium-bottom">
                   Random Thoughts
               </h1>
-              <p className="uk-text-left">It started with an obsession for colours and gradients. There are millions of possible colours and gradient combination but we are hardly able to explore even 10% of it. There was a need for something which can make us explore every possibility of colours and gradients. With this thought, <span className="gradient-text">Random Gradients</span> was born.</p>
+              <p className="uk-text-left">It started with an obsession for colours and gradients. There are millions of possible colours and gradient combination but we are hardly able to explore even fraction of it. There was a need for something which can make us explore every possibility of colours and gradients. With this thought, <span className="gradient-text">Random Gradients</span> was born.</p>
               <p className="uk-text-left"> <span className="gradient-text">Random Gradients</span>  gives a designer a freedom to explore an infinite possibility of colours and gradients. It also enables them to save random gradients for further use in their design. </p>
           </div>
           </div>
