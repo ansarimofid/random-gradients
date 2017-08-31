be8f5989a39f1f3abf78f983a794b99bbe837f98
ansarimofid <ansarimofid@gmail.com>
2017-08-31 16:56:57 +0530

    Adds Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ansarimofid on 31/08/17.
+ */
+import React from 'react'
+
+const Footer = () => (
+  <footer className="uk-margin-medium-top uk-margin-medium-bottom">
+    <div className="uk-text-center">
+      <span>Designed & Developed by <a className="uk-text-danger" href="http://ansarimofid.in" target="blank">Mofid Ansari</a></span> | Open sourced at <a className="uk-text-danger" href="https://github.com/ansarimofid/random-gradients"
+                           target="blank">Github</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
